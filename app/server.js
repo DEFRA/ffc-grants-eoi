2886aef594d8b3bd2cb0ac9d0d6022ab5cd2eed9
Paul Andrews <paul.andrews@defra.gov.uk>
2020-08-24 12:38:27 +0100

    Smelly code

--- app/server.js
+++ app/server.js
@@ -20,9 +20,7 @@ async function createServer () {
         compile: (src, options) => {
           const template = nunjucks.compile(src, options.environment)
 
-          return (context) => {
-            return template.render(context)
-          }
+          return context => template.render(context)
         }
       }
     },
