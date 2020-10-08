aabcad30a85e5866b60e19fdeb4a97ba94cd27d5
Paul Andrews <paul.andrews@defra.gov.uk>
2020-10-08 12:40:29 +0100

    Send magic link in message

--- app/routes/confirmation.js
+++ app/routes/confirmation.js
@@ -10,6 +10,8 @@ module.exports = {
     console.log(`Email Address: ${request.yar.get('emailAddress')}`)
     console.log(`ConfirmationID: ${confirmationId}`)
 
+    const httpPrefix = process.env.NODE_ENV === 'production' ? 'https' : 'http'
+    const magicLink = `${httpPrefix}://${process.env.SITE_URL}/check-details?confirmationId=${confirmationId}`
     const messageService = await require('../services/message-service')
 
     try {
@@ -18,7 +20,8 @@ module.exports = {
           confirmationId: confirmationId.toString(),
           inEngland: request.yar.get('inEngland') === 'yes',
           businessName: request.yar.get('businessName'),
-          emailAddress: request.yar.get('emailAddress')
+          emailAddress: request.yar.get('emailAddress'),
+          magicLink: magicLink
         })
       )
     } catch (err) {
@@ -30,13 +33,11 @@ module.exports = {
       })
     }
 
-    const httpPrefix = process.env.NODE_ENV === 'production' ? 'https' : 'http'
-
     return h.view('confirmation', {
       output: {
         titleText: 'EOI submitted',
         html: `Your reference number<br><strong>${confirmationId}</strong>`,
-        link: `${httpPrefix}://${process.env.SITE_URL}/check-details?confirmationId=${confirmationId}`
+        link: magicLink
       }
     })
   }
