1909801eb67e2898eea5324b724a2fe5161f6476
Paul Andrews <paul.andrews@defra.gov.uk>
2020-10-19 10:57:20 +0100

    Generate ref for saved apps

--- app/routes/progress-reference.js
+++ app/routes/progress-reference.js
@@ -4,9 +4,29 @@ module.exports = [
   {
     method: 'GET',
     path: `/${pageName}`,
-    handler: (request, h) => {
+    handler: async (request, h) => {
+      const confirmationId = `${Math.floor(Math.random() * 100000000)}`
+      const messageService = await require('../services/message-service')
+
+      try {
+        await messageService.publishEOI(
+          JSON.stringify({
+            confirmationId: confirmationId,
+            inEngland: request.yar.get('inEngland') === 'yes'
+          })
+        )
+      } catch (err) {
+        return h.view('confirmation', {
+          output: {
+            titleText: 'EOI not submitted',
+            html: 'Error sending EOI'
+          }
+        })
+      }
+
       return h.view('progress-reference', {
-        backLink: '/'
+        backLink: '/',
+        progressReference: confirmationId
       })
     }
   }
